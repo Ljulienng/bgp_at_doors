81bbd04d75af1a3da2e875c8d429c5c3ea5cd518
jnguyen <jnguyen@ubuntu.myguest.virtualbox.org>
2023-12-13 12:58:42 +0100

    ÃP3 done

---
+++ P3/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Apply the configuration to the containers
+running_containers=$(docker ps -q)
+if [[ ! -z $running_containers ]]; then
+    for i in ${running_containers[@]}; do
+        container_info=$(echo "$i:$(docker exec $i hostname)")
+        hostname=${container_info#*:}
+        container_id=${container_info%:*}
+        case $hostname in
+            host_jnguyen-*|router_jnguyen-*)
+                filename="$hostname"
+                docker cp "./confs/$filename" "$container_id:/"
+                docker exec "$container_id" ash "/$filename"
+                echo "Configuration is applied on $container_id running the $hostname"
+                ;;
+        esac
+    done
+    exit 0
+else
+    echo "No running containers"
+    exit 1
+fi
---
+++ setup_vm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo add-apt-repository -y ppa:gns3/ppa
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin gns3-gui gns3-server
+
+# Add current user to the following groups
+sudo usermod -aG kvm,libvirt,docker,ubridge,wireshark $USER
